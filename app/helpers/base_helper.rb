26261d09e2a685d18369500e3591cd67446bfe30
Bruno Bornsztein <bruno@otis.local>
2008-04-01 22:03:13 -0500

    more style changes

--- app/helpers/base_helper.rb
+++ app/helpers/base_helper.rb
@@ -292,7 +292,7 @@ module BaseHelper
 
   def feed_icon_tag(title, url)
     (@feed_icons ||= []) << { :url => url, :title => title }
-    link_to image_tag('feed-icon.png', :size => '14x14', :alt => "Subscribe to #{title}", :plugin => 'community_engine'), url
+    link_to image_tag('feed.png', :size => '14x14', :alt => "Subscribe to #{title}", :plugin => 'community_engine'), url
   end
 
   def search_posts_title
