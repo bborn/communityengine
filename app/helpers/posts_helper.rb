e561996e99b885ae9bc770a3aed6edecf4f06b4a
Bruno Bornsztein <bruno@otis.local>
2008-04-08 10:55:29 -0500

    only show the post ad on posts with more than four graphs

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,13 +1,18 @@
 module PostsHelper
 
   def post_with_ad_in_content(post)
-    string = Ad.display(:post_content, logged_in?)      
+    # string = Ad.display(:post_content, logged_in?)      
+    string = "test ad"
 
     doc = Hpricot(post.post)
-    doc.search("p").each_with_index do |p,i|
-      if i.eql?(2)
-        p.before string 
-        p[:id] = "jump"
+    paragraphs = doc.search("p")
+    
+    if paragraphs.length > 4
+      paragraphs.each_with_index do |p,i|
+        if i.eql?(2)
+          p.before string 
+          p[:id] = "jump"
+        end
       end
     end
     
