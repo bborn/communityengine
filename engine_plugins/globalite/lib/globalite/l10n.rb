6812988ec0230c94a07433f3e37a46baf5e5cd82
Bruno Bornsztein <bruno@otis.local>
2008-07-16 14:11:58 -0500

    pulled out globalite extensions from the plugin, thanks Bryan

--- engine_plugins/globalite/lib/globalite/l10n.rb
+++ engine_plugins/globalite/lib/globalite/l10n.rb
@@ -1,9 +1,6 @@
 module Globalite 
 
-  module L10n
-    @@show_localization_keys_for_debugging = false
-    attr_accessor :show_localization_keys_for_debugging    
-    
+  module L10n   
     @@default_language = :en
     attr_reader :default_language
 
