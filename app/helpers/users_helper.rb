407943cba2fd8be8792690cd36be4cbb8b7f21ca
Bruno Bornsztein <bruno@otis.local>
2008-07-24 12:58:09 -0500

    en-US locale was missing 'cancel' text

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,17 +4,9 @@ module UsersHelper
   end    
   
   def random_greeting(user)
-    greetings = [:greeting_1,
-    :greeting_2,
-    :greeting_3,
-    :greeting_4,
-    :greeting_5,
-    :greeting_6,
-    :greeting_7,
-    :greeting_8,
-    :greeting_9,
-    :greeting_10]
-    return greetings.sort_by {rand}.first().l(:user => user.login)
+    greetings = ['Hello', 'Hola', 'Hi ', 'Yo', 'Welcome back,', 'Greetings',
+        'Wassup', 'Aloha', 'Halloo']
+    "#{greetings.sort_by {rand}.first} #{user.login}!"
   end
   
 end
