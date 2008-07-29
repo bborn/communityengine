8e5f0346fdc0c6704e1e2a30e484481596caa53e
Bruno Bornsztein <bruno@otis.local>
2008-07-29 15:38:06 -0500

    localizing some strings that got skipped

--- app/helpers/base_helper.rb
+++ app/helpers/base_helper.rb
@@ -194,16 +194,16 @@ module BaseHelper
 
   def add_friend_link(user = nil)
 		html = "<span class='friend_request' id='friend_request_#{user.id}'>"
-    html += link_to_remote "Request friendship!",
+    html += link_to_remote "Request friendship!".l,
 				{:update => "friend_request_#{user.id}",
 					:loading => "$$('span#friend_request_#{user.id} span.spinner')[0].show(); $$('span#friend_request_#{user.id} a.add_friend_btn')[0].hide()", 
 					:complete => visual_effect(:highlight, "friend_request_#{user.id}", :duration => 1),
-          500 => "alert('Sorry, there was an error requesting friendship')",
+          500 => "alert('"+'Sorry, there was an error requesting friendship'.l+"')",
 					:url => hash_for_user_friendships_url(:user_id => current_user.id, :friend_id => user.id), 
 					:method => :post }, {:class => "add_friend button"}
 		html +=	"<span style='display:none;' class='spinner'>"
 		html += image_tag 'spinner.gif', :plugin => "community_engine"
-		html += " Requesting friendship...</span></span>"
+		html += "Requesting friendship".l+" ...</span></span>"
 		html
   end
 
