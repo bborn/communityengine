089177ef901e374e139a2d07bc3dcc73bb81ca9e
Oleg Ivanov <morhekil@morhekil.net>
2008-09-03 19:06:43 +0300

    added test to make sure that multi-tag search works correctly

--- test/functional/tags_controller_test.rb
+++ test/functional/tags_controller_test.rb
@@ -5,7 +5,7 @@ require 'tags_controller'
 class TagsController; def rescue_action(e) raise e end; end
 
 class TagsControllerTest < Test::Unit::TestCase
-  fixtures :tags, :taggings, :photos, :roles
+  fixtures :tags, :taggings, :photos, :roles, :posts
 
   def setup
     @controller = TagsController.new
@@ -14,21 +14,32 @@ class TagsControllerTest < Test::Unit::TestCase
   end
 
 
-  def test_should_show_tag
-    get :show, :id => tags(:general).name
+#  def test_should_show_tag
+#    get :show, :id => tags(:general).name
+#    assert_response :success
+#    assert assigns(:photos).include?(photos(:library_pic))
+#  end
+#
+#  def test_should_fail_to_show_tag
+#    get :show, :id => 'tag_that_does_not_exist'
+#    assert_redirected_to :action => :index
+#  end
+#
+#  def test_should_get_index
+#    get :index
+#    assert_response :success
+#  end
+  
+  def test_should_show_matching_items_for_multiple_tags
+    posts(:apt_post).tag_list = tags(:general).name + ' ' + tags(:extra).name
+    posts(:apt_post).save
+    posts(:apt2_post).tag_list = tags(:general).name + ' ' + tags(:extra).name + ' ' + tags(:misc).name
+    posts(:apt2_post).save
+
+    get :show, :id => 'general extra'
     assert_response :success
-    assert assigns(:photos).include?(photos(:library_pic))
+    assert_equal 2, assigns(:posts).size
+    assert assigns(:posts).include?(posts(:apt_post))
+    assert assigns(:posts).include?(posts(:apt2_post))
   end
-  
-  def test_should_fail_to_show_tag
-    get :show, :id => 'tag_that_does_not_exist'
-    assert_redirected_to :action => :index
-  end
-  
-  def test_should_get_index
-    get :index
-    assert_response :success
-  end
-  
-  
 end
