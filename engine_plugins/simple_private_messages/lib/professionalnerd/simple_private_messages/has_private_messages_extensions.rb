dbc81f645c522deb421423cac2dbe8b8f6fb493c
Bruno Bornsztein <bruno@otis.local>
2009-01-02 11:38:58 -0600

    fixing up recent forum posts view

--- engine_plugins/simple_private_messages/lib/professionalnerd/simple_private_messages/has_private_messages_extensions.rb
+++ engine_plugins/simple_private_messages/lib/professionalnerd/simple_private_messages/has_private_messages_extensions.rb
@@ -46,7 +46,7 @@ module Professionalnerd #:nodoc:
         
         # Returns the number of unread messages for this user
         def unread_message_count
-          eval options[:class_name] + '.count(:conditions => ["recipient_id = ? AND read_at IS NULL", self])'
+          eval options[:class_name] + '.count(:conditions => ["recipient_id = ? AND recipient_deleted IS NULL AND read_at IS NULL", self])'
         end
       end 
     end
