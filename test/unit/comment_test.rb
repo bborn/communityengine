54814c3b3893212cc958d732ea5abc26df42ef7e
Bruno Bornsztein <bruno@bruno-bornszteins-macbook-pro.local>
2009-01-08 15:10:19 -0600

    notify non-logged-in commenters when follow ups are posted

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -18,11 +18,25 @@ class CommentTest < Test::Unit::TestCase
   def test_should_be_created_anonymously
     AppConfig.allow_anonymous_commenting = true
     assert_difference Comment, :count, 1 do
-      comment = Comment.create!(:comment => 'foo', :author_email => 'bar@foo.com', :author_ip => '123.123.123', :recipient => users(:quentin))
+      comment = Comment.create!(:comment => 'foo', :author_email => 'bar@foo.com', :author_ip => '123.123.123', :recipient => users(:quentin), :commentable => users(:quentin))
     end
     AppConfig.allow_anonymous_commenting = false
   end
+  
+  def test_should_notify_previous_anonymous_commenter
+    AppConfig.allow_anonymous_commenting = true
+    Comment.create!(:comment => 'foo', :author_email => 'bruno@foo.com', :author_ip => '123.123.123', :recipient => users(:quentin), :commentable => users(:quentin))
+    Comment.create!(:comment => 'bar', :author_email => 'bruno@foo.com', :author_ip => '123.123.123', :recipient => users(:quentin), :commentable => users(:quentin))    
 
+    comment = Comment.create!(:comment => 'bar', :author_email => 'alicia@foo.com', :author_ip => '123.123.123', :recipient => users(:quentin), :commentable => users(:quentin))        
+
+    assert_difference ActionMailer::Base.deliveries, :length, 1 do
+      comment.notify_previous_anonymous_commenters
+    end
+    AppConfig.allow_anonymous_commenting = false
+  end  
+  
+  
   def test_should_not_be_created_anonymously
     assert_no_difference Comment, :count do
       comment = Comment.create(:comment => 'foo', :author_email => 'bar@foo.com', :author_ip => '123.123.123', :recipient => users(:quentin))
