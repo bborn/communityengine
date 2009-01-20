f72ba342e3966c2cfa682c77102e70175874205f
Johannes A Kaefer <jak4@gmx.net>
2009-01-20 20:19:45 +0100

    This is now a Postgres-compatible Version of CE. Breaking migration path, since modifications to migrations.

--- db/migrate/059_create_messages.rb
+++ db/migrate/059_create_messages.rb
@@ -2,7 +2,10 @@ class CreateMessages < ActiveRecord::Migration
   def self.up
     create_table :messages do |t|
       t.integer :sender_id, :recipient_id
-      t.boolean :sender_deleted, :recipient_deleted, :default => 0
+      #t.boolean :sender_deleted, :recipient_deleted, :default => 0
+      # workaround for postgres users until rails bug 1036 is fixed
+      # uncomment the line below and comment the line above, see also 061_postgres_compatibility_changes.rb
+      t.boolean :sender_deleted, :recipient_deleted, :default => false
       t.string :subject
       t.text :body
       t.datetime :read_at
