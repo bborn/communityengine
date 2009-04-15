47b5887da5b7a08263a4465d76f3f5bd2a198ced
bborn <bruno.bornsztein@gmail.com>
2009-04-15 09:14:55 -0500

    don't use strict medi embedding with MCE (screws up embeds)

--- engine_config/initializers/mce_options.rb
+++ engine_config/initializers/mce_options.rb
@@ -20,7 +20,8 @@ AppConfig.default_mce_options = {
   :plugin_preview_width => "950",
   :plugin_preview_height => "650",
   :editor_deselector => "mceNoEditor",
-  :extended_valid_elements => "img[class|src|flashvars|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|obj|param|embed|scale|wmode|salign|style],embed[src|quality|scale|salign|wmode|bgcolor|width|height|name|align|type|pluginspage|flashvars],object[align<bottom?left?middle?right?top|archive|border|class|classid|codebase|codetype|data|declare|dir<ltr?rtl|height|hspace|id|lang|name|style|tabindex|title|type|usemap|vspace|width]"  
+  :extended_valid_elements => "img[class|src|flashvars|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|obj|param|embed|scale|wmode|salign|style],embed[src|quality|scale|salign|wmode|bgcolor|width|height|name|align|type|pluginspage|flashvars],object[align<bottom?left?middle?right?top|archive|border|class|classid|codebase|codetype|data|declare|dir<ltr?rtl|height|hspace|id|lang|name|style|tabindex|title|type|usemap|vspace|width]",
+	:media_strict => false    
   }
   
 AppConfig.simple_mce_options = {
