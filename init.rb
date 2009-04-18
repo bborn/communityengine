52c3e3068c165397c24b9437bef1c9cb49c0423d
bborn <bruno.bornsztein@gmail.com>
2009-04-18 18:54:17 -0500

    more migration stuff

--- init.rb
+++ init.rb
@@ -1,3 +1,32 @@
+#hack the desert plugin to allow generating plugin migrations
+Desert::Plugin.class_eval do
+  def latest_migration
+    migrations.last
+  end
+  
+  # Returns the version numbers of all migrations for this plugin.
+  def migrations
+    migrations = Dir[migration_path+"/*.rb"]
+    migrations.map { |p| File.basename(p).match(/0*(\d+)\_/)[1].to_i }.sort
+  end    
+end
+# Fix Desert's 'current_version' which tries to order by version desc, but version is a string type column, so it breaks
+# sort the rows in ruby instead to make sure we get the highest numbered version
+Desert::PluginMigrations::Migrator.class_eval do
+  class << self
+    def current_version #:nodoc:
+      result = ActiveRecord::Base.connection.select_values("SELECT version FROM #{schema_migrations_table_name} WHERE plugin_name = '#{current_plugin.name}'").map(&:to_i).sort.reverse[0]
+      if result
+        result
+      else
+        # There probably isn't an entry for this plugin in the migration info table.
+        0
+      end
+    end
+  end
+end
+
+
 require 'community_engine'
 require 's3_cache_control'
 
@@ -41,30 +70,3 @@ module ApplicationConfiguration
 end
 
 
-#hack the desert plugin to allow generating plugin migrations
-Desert::Plugin.class_eval do
-  def latest_migration
-    migrations.last
-  end
-  
-  # Returns the version numbers of all migrations for this plugin.
-  def migrations
-    migrations = Dir[migration_path+"/*.rb"]
-    migrations.map { |p| File.basename(p).match(/0*(\d+)\_/)[1].to_i }.sort
-  end    
-end
-# Fix Desert's 'current_version' which tries to order by version desc, but version is a string type column, so it breaks
-# sort the rows in ruby instead to make sure we get the highest numbered version
-Desert::PluginMigrations::Migrator.class_eval do
-  class << self
-    def current_version #:nodoc:
-      result = ActiveRecord::Base.connection.select_values("SELECT version FROM #{schema_migrations_table_name} WHERE plugin_name = '#{current_plugin.name}'").map(&:to_i).sort.reverse[0]
-      if result
-        result
-      else
-        # There probably isn't an entry for this plugin in the migration info table.
-        0
-      end
-    end
-  end
-end
--- lib/community_engine.rb
+++ lib/community_engine.rb
@@ -10,27 +10,27 @@ module CommunityEngine
   class << self
 
     def check_for_pending_migrations
-    #   newest_ce_migration = Engines.plugins[:community_engine].latest_migration
-    #   current_ce_version  = guess_current_ce_version
-    # 
-    #   pending = newest_ce_migration - current_ce_version
-    #   if pending > 0
-    #     puts "---"        
-    #     puts "[COMMUNITY ENGINE] You have #{pending} pending CommunityEngine migrations:"
-    #     puts "CE is at #{newest_ce_migration}, but you have only migrated it to #{current_ce_version}"
-    #     puts "Please run 'script/generate plugin_migration' AND 'rake db:migrate' before continuing, or you will experience errors."
-    #     puts "---"
-    #   end      
-    # end
-    # 
-    # def guess_current_ce_version
-    #   # DUMB: checks your db/migrate and parses out the last CE migration to find out which version you're at      
-    #   last_version =  Dir["#{RAILS_ROOT}/db/migrate/[0-9]*_community_engine_to*.rb"].sort.last
-    #   if last_version
-    #     last_version[/.*_community_engine_to_version_(\d+)/, 1].to_i
-    #   else
-    #     0
-    #   end
+      newest_ce_migration = Desert::Manager.find_plugin('community_engine').latest_migration
+      current_ce_version  = guess_current_ce_version
+    
+      pending = newest_ce_migration - current_ce_version
+      if pending > 0
+        puts "---"        
+        puts "[COMMUNITY ENGINE] You have #{pending} pending CommunityEngine migrations:"
+        puts "CE is at #{newest_ce_migration}, but you have only migrated it to #{current_ce_version}"
+        puts "Please run 'script/generate plugin_migration' AND 'rake db:migrate' before continuing, or you will experience errors."
+        puts "---"
+      end      
+    end
+    
+    def guess_current_ce_version
+      # DUMB: checks your db/migrate and parses out the last CE migration to find out which version you're at      
+      last_version =  Dir["#{RAILS_ROOT}/db/migrate/[0-9]*_community_engine_to*.rb"].sort.last
+      if last_version
+        last_version[/.*_community_engine_to_version_(\d+)/, 1].to_i
+      else
+        0
+      end
     end
     
   end
