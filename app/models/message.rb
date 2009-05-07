f8f669b18dbc02c331d5e6bb3cc0b98aab29c614
bborn <bruno.bornsztein@gmail.com>
2009-05-07 14:41:15 -0500

    inline replying to messages on show page

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,7 @@ class Message < ActiveRecord::Base
   is_private_message
   
   attr_accessor :to
+  attr_accessor :reply_to
 
   validates_presence_of :body, :subject
   validates_presence_of :recipient, :message => "is invalid"
@@ -15,6 +16,22 @@ class Message < ActiveRecord::Base
   
   def notify_recipient
     UserNotifier.deliver_message_notification(self)
+  end
+  
+  def self.new_reply(user, params = {})
+    message = new(params[:message])
+
+    if params[:reply_to]
+      reply_to = user.received_messages.find(params[:reply_to])
+      unless reply_to.nil?
+        message.reply_to = reply_to
+        message.to = reply_to.sender.login
+        message.subject = "Re: #{reply_to.subject}"
+        message.body = "\n\n*Original message*\n\n #{reply_to.body}"
+      end
+    end
+    
+    message
   end
   
 end
