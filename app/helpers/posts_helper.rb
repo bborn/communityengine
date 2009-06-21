64e560cc6e5c2bf96e0c9033232bd3e7792e6084
eksatx <communityengine.eksatx@mamber.net>
2009-06-20 20:40:42 -0500

    Tweak ShareThis settings to work better with Facebook and MySpace

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -18,5 +18,18 @@ module PostsHelper
     doc.to_html
   end
 
+  # The ShareThis widget defines a bunch of attributes you can customize.
+  # Facebook seems to ignore them (it uses title and description meta tags
+  # instead).  MySpace, however, only works if you set these attributes.
+  def sharethis_options(post)
+    content_tag :script, :type=>"text/javascript" do   
+      <<-eos
+	SHARETHIS.addEntry({
+		title:'#{escape_javascript(post.title)}',
+                content:'#{escape_javascript(truncate_words(post.post, 75, '...' ))}'
+	}, {button:true});
+      eos
+    end
+  end
 
 end
