4dc634265c7c756b8da707dc2e40099716e9476a
bborn <bruno.bornsztein@gmail.com>
2009-11-19 13:06:08 -0600

    oops, fixed that last commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,8 @@ class SessionsController < BaseController
     @user_session = UserSession.new(:login => params[:login], :password => params[:password], :remember_me => params[:remember_me])
 
     if @user_session.save
-      current_user = current_user_session.record #if current_user has been called before this, it will ne nil, so we have to make to reset it
+
+      current_user = @user_session.record #if current_user has been called before this, it will ne nil, so we have to make to reset it
       
       flash[:notice] = :thanks_youre_now_logged_in.l
       redirect_back_or_default(dashboard_user_path(current_user))
