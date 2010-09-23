11a8bb181e8e08f3846368e39c63fb3a1cdd8530
bborn <bruno.bornsztein@gmail.com>
2010-09-23 14:39:46 -0500

    Revert "fix i18n"
    This reverts commit 286bf6474cc1f3f5fc708ca45d89b7ebdb853f2d.
    
    Signed-off-by: bborn <bruno.bornsztein@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -182,7 +182,7 @@ class Post < ActiveRecord::Base
   end  
 
   def published_at_display(format = 'published_date')
-    is_live? ? published_at.strftime(I18n.t("time.formats.#{format.to_s}")) : 'Draft'
+    is_live? ? I18n.l(published_at, :format => format) : 'Draft'
   end
       
 end
