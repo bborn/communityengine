47bec9f1505f7a8f968c439b214456d07b48c870
bborn <bruno.bornsztein@gmail.com>
2011-02-13 08:35:45 -0600

    fix moderatorship tests

--- app/models/moderatorship.rb
+++ app/models/moderatorship.rb
@@ -2,6 +2,7 @@ class Moderatorship < ActiveRecord::Base
   belongs_to :forum
   belongs_to :user
   validates_presence_of :user, :forum
-  
-  before_create { |r| count(:all, :conditions => ['forum_id = ? and user_id = ?', r.forum_id, r.user_id]).zero? }
+
+  validates_uniqueness_of :user_id, :scope => :forum_id
+
 end
