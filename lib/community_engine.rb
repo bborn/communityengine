d3945504fa88a408a304040b394d068a3303156d
greendog <greendog@greendog.ru>
2011-08-21 14:09:09 +0400

    localization support has been moved to the outer gem (locales)

--- lib/community_engine.rb
+++ lib/community_engine.rb
@@ -1,8 +1,7 @@
 require 'community_engine/engine'
 
 require 'community_engine/authenticated_system'
-require 'community_engine/localized_application'  
-require 'community_engine/community_engine_sha1_crypto_method'    
+require 'community_engine/community_engine_sha1_crypto_method'
 require 'community_engine/i18n_extensions'  
 require 'community_engine/viewable'    
 require 'community_engine/url_upload'
--- lib/community_engine/util/version.rb
+++ lib/community_engine/util/version.rb
@@ -1,6 +1,5 @@
 module CommunityEngine
   def self.version
-    #File.read('../../VERSION')
     File.read(File.expand_path("../../../../VERSION", __FILE__)).strip
   end
 end
