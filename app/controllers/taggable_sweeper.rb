f14fd58f482d53ff79c524e96d7a747dd42a63d7
bruno bornsztein <bruno.bornsztein@gmail.com>
2011-10-03 20:53:24 -0500

    bugfix for taggable sweeper

--- app/controllers/taggable_sweeper.rb
+++ app/controllers/taggable_sweeper.rb
@@ -30,7 +30,7 @@ class TaggableSweeper < ActionController::Caching::Sweeper
   def expire_cache_for(record)
     # Expire the tag show pages
     record.tags.each do |tag|
-      expire_action(tag_url(tag))
+      expire_action({:action=>"show", :controller=>"tags", :id => tag.id})      
     end
   end
   
