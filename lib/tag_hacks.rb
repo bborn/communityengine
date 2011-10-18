a23f644dfc3142e25b19bc7de1a9d7ab72ea63a4
bruno bornsztein <bruno.bornsztein@gmail.com>
2011-10-18 07:13:25 -0500

    fix tag.popular

--- lib/tag_hacks.rb
+++ lib/tag_hacks.rb
@@ -1,3 +1,13 @@
+Tag.instance_eval do
+
+  def popular(limit = 20, type = nil)
+    tags = Tag.counts(:at_least => 0).limit(limit).order('count DESC')
+    tags = tags.where("taggings.taggable_type = ?", type.capitalize) if type
+    tags
+  end  
+  
+end
+
 Tag.class_eval do
     
   def to_param
@@ -10,11 +20,5 @@ Tag.class_eval do
   
     Tag.where("tags.id != '#{self.id}'").select("tags.id, tags.name, COUNT(tags.id) as count").joins(:taggings).where({:taggings => {:taggable_id => taggable_ids}}).group("tags.id, tags.name").order("count DESC").limit(limit)
   end
-  
-  def self.popular(limit = 20, type = nil)
-    tags = Tag.counts(:at_least => 0).limit(limit).order('count DESC')
-    tags = tags.where("taggings.taggable_type = ?", type.capitalize) if type
-    tags
-  end  
-  
+    
 end
