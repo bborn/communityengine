2aebc6ecced9b6f87bd27dfb8401d69414bacd64
bruno bornsztein <bruno.bornsztein@gmail.com>
2011-11-18 08:19:14 -0600

    bump version to beta2

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -3,7 +3,7 @@ module CommunityEngine
      MAJOR = 2
      MINOR = 0
      PATCH = 0
-     BUILD = 'beta1'
+     BUILD = 'beta2'
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
    end
