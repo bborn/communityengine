7bf6baff8653ed9f5ef7298aa38838c2fa36e018
bruno bornsztein <bruno.bornsztein@gmail.com>
2012-02-14 07:44:24 -0600

    beta2

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -3,7 +3,7 @@ module CommunityEngine
      MAJOR = 2
      MINOR = 0
      PATCH = 0
-     BUILD = 'beta2'
+     BUILD = 'beta3'
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
    end
