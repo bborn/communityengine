48ecef1801c86d808e4b6c6139816353e5fdc078
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-08-26 19:14:07 -0600

    Made fields accessible so that categories could be submitted.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,12 +2,16 @@ class Category < ActiveRecord::Base
   has_many :posts, :order => "published_at desc"
   validates_presence_of :name
   
-  attr_accessible :name
+  attr_accessible :name, :tips, :new_post_text, :nav_text
   
   has_friendly_id :name, :use_slug => true
       
   def display_new_post_text
-    new_post_text
+    if new_post_text.blank?
+      false      
+    else
+      new_post_text
+    end
   end
   
 end
