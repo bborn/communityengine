59c613f0380bf7800fd7cf19410a92156b94c3be
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-08-29 20:44:55 -0600

    Removing plugins for now.  Will add jquery compatible replacements once the base tinymce is working correctly.

--- app/assets/javascripts/tinymce/plugins/curblyadvimage/langs/en.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('en.curblyadvimage',{
-  image_desc : 'Upload or insert an image'
-});
--- app/assets/javascripts/tinymce/plugins/tinyautosave/langs/da.js
+++
@@ -1,5 +0,0 @@
-﻿tinyMCE.addI18n('da.tinyautosave',{
-restore_content: "Gendan sikkerhedskopi",
-no_content: "Der er ingen sikkerhedskopi tilgængelig",
-warning_message: "Hvis du gendanner sikkerhedskopien, vil du miste alt det indhold, der i øjeblikket er i editoren.\n\nEr du sikker på at du vil gendanne sikkerhedskopien?"
-});
