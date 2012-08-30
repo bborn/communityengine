3aece5d2a110ffcb15acd88d60bf9418ac61ffdd
Karen Lundgren <karen.e.lundgren@gmail.com>
2012-08-29 21:42:54 -0600

    Removing plugins line altogether to debug

--- config/initializers/mce_options.rb
+++ config/initializers/mce_options.rb
@@ -15,7 +15,6 @@ configatron.default_mce_options = {
   :theme_advanced_buttons1 => %w{bold italic underline separator justifyleft justifycenter justifyright indent outdent separator bullist numlist separator link unlink image media separator undo redo code},
   :theme_advanced_buttons2 => [],
   :theme_advanced_buttons3 => [],
-  :plugins => %w{media inlinepopups safari},
   :editor_deselector => "mceNoEditor",
   :editor_selector => "rich_text_editor",  
   :extended_valid_elements => "img[class|src|flashvars|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name|obj|param|embed|scale|wmode|salign|style],embed[src|quality|scale|salign|wmode|bgcolor|width|height|name|align|type|pluginspage|flashvars],object[align<bottom?left?middle?right?top|archive|border|class|classid|codebase|codetype|data|declare|dir<ltr?rtl|height|hspace|id|lang|name|style|tabindex|title|type|usemap|vspace|width]",
@@ -35,6 +34,5 @@ configatron.simple_mce_options = {
   :theme_advanced_buttons1 => %w{bold italic underline separator bullist numlist separator link unlink image},
   :theme_advanced_buttons2 => [],
   :theme_advanced_buttons3 => [],
-  :plugins => %w{inlinepopups safari},
   :editor_selector => "rich_text_editor"
   }
