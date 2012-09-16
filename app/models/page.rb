f01c158d10b65e12074e709a18e33e19c9ac6001
Polar Humenn <polar@adiron.com>
2012-09-16 12:34:41 -0400

    Added Accessible Attributes for page creation and update.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,9 @@
 class Page < ActiveRecord::Base
   acts_as_publishable :live, :draft, :members_only, :public
   validates_presence_of :title, :body
-  
+
+  attr_accessible :title, :body, :page_public, :published_as
+
   def to_param
     id.to_s << "-" << (title ? title.parameterize : '')
   end
