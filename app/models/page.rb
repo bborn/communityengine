1893edc4afd903a7bae13f33263839bc7f7a7f77
bruno bornsztein <bruno.bornsztein@gmail.com>
2013-03-25 09:00:04 -0500

    att accessible on page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,9 @@
 class Page < ActiveRecord::Base
   acts_as_publishable :live, :draft, :members_only, :public
   validates_presence_of :title, :body
-  
+
+  attr_accessible :title, :body, :published_as, :page_public
+
   def to_param
     id.to_s << "-" << (title ? title.parameterize : '')
   end
