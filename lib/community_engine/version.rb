fe97dd5133cba2851325543961825dcd6e50c3a4
Polar Humenn <polar@adiron.com>
2013-08-26 13:02:04 -0400

    Bump Version

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -2,7 +2,7 @@ module CommunityEngine
   module Version
      MAJOR = 3
      MINOR = 0
-     PATCH = 0
+     PATCH = 1
      BUILD = "pre1"
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
