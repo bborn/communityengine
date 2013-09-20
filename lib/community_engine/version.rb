bb7ca2bae447a3c3f7698670a02bdf1a09d4b570
Polar Humenn <polar@adiron.com>
2013-09-20 15:28:15 -0400

    Upgraded to tinyMCE 4.02

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -2,8 +2,8 @@ module CommunityEngine
   module Version
      MAJOR = 3
      MINOR = 0
-     PATCH = 1
-     BUILD = "pre1"
+     PATCH = 2
+     BUILD = "pre2"
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
    end
