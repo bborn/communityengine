2c39331ccfe31f9dc9e0ddbbf492d98cc6677c7c
Robert James Kaes <rkaes@ePublishing.com>
2013-10-11 12:49:59 -0400

    Increment the Version String

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -2,7 +2,7 @@ module CommunityEngine
   module Version
      MAJOR = 2
      MINOR = 1
-     PATCH = 0
+     PATCH = 1
      BUILD = nil
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
