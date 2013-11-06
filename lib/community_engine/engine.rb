11fa9ae0d857a83d54970bd311a3c15a3d84ed73
bruno bornsztein <bruno.bornsztein@gmail.com>
2013-11-06 16:41:56 -0600

    bump minor version

--- lib/community_engine/engine.rb
+++ lib/community_engine/engine.rb
@@ -7,35 +7,22 @@ module CommunityEngine
 
     initializer engine_name do |app|
       require root.join('config','application_config.rb')
-      require app.root.join('config','application_config.rb')            
+      require app.root.join('config','application_config.rb')
     end
-    
-    initializer "#{engine_name}.initializers", :before => :load_config_initializers do       
-      Dir["#{root}/config/initializers/**/*.rb"].each do |initializer| 
+
+    initializer "#{engine_name}.initializers", :before => :load_config_initializers do
+      Dir["#{root}/config/initializers/**/*.rb"].each do |initializer|
         load(initializer) unless File.exists?("#{root.to_s}/config/initializers/#{File.basename(initializer)}")
-      end                
+      end
     end
-        
+
     initializer "#{engine_name}.rakismet_config", :before => "rakismet.setup" do |app|
       if !configatron.akismet_key.nil?
         app.config.rakismet.key  = configatron.akismet_key
         app.config.rakismet.url  = configatron.app_host
       end
     end
-    
-    initializer "#{engine_name}.load_middleware", :after => :load_config_initializers do
-      if !configatron.auth_providers.nil?
-        configatron.protect(:auth_providers)
-        configatron.auth_providers.to_hash.each do |name, hash|
-          provider = "::OmniAuth::Strategies::#{name.to_s.classify}".constantize
-          config.app_middleware.use provider, hash[:key], hash[:secret]          
-        end
-      end
-    end
-    
-    
 
-     
   end
 end
 
--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -2,7 +2,7 @@ module CommunityEngine
   module Version
      MAJOR = 2
      MINOR = 3
-     PATCH = 0
+     PATCH = 1
      BUILD = nil
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
