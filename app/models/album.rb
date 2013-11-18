abb45120dd78f6bb588e51744b31f1a3ee2ce8ba
bruno bornsztein <bruno.bornsztein@gmail.com>
2013-11-18 16:40:27 -0600

    add moderated comment macros

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,8 +3,8 @@ class Album < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user_id
   acts_as_activity :user
-  acts_as_commentable
-  validates_presence_of :title  
+  acts_as_moderated_commentable
+  validates_presence_of :title
 
   attr_accessible :title, :description
 
