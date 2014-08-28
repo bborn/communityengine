366c71f0d80a39efa4f41701dac39b9618741d66
bruno bornsztein <bruno.bornsztein@gmail.com>
2014-08-28 13:27:18 -0500

    bump version to 2.3.2

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -2,7 +2,7 @@ module CommunityEngine
   module Version
      MAJOR = 2
      MINOR = 3
-     PATCH = 1
+     PATCH = 2
      BUILD = nil
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
