996247f801e45d3947e5704dbfcbb64f97cf020b
Trevor <trevorjbullock@gmail.com>
2014-09-30 12:41:27 -0700

    Fix deprecation warning during test.

--- test/testapp/config/application.rb
+++ test/testapp/config/application.rb
@@ -31,6 +31,7 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
 
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
