3a457cc80d06456c4f21f591bc5b6bf599f54b70
Robert James Kaes <rkaes@ePublishing.com>
2014-10-08 09:40:00 -0400

    Bump Version Number

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -2,7 +2,7 @@ module CommunityEngine
   module Version
      MAJOR = 2
      MINOR = 1
-     PATCH = 1
+     PATCH = 2
      BUILD = "epublishing"
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
