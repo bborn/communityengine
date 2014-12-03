977bd864ef005cb4ea730a7c0b6ea265937d8804
Trevor Bullock <trevorjbullock@gmail.com>
2014-12-02 23:11:05 -0800

    Bump version to 3.0.0.

--- lib/community_engine/version.rb
+++ lib/community_engine/version.rb
@@ -2,8 +2,8 @@ module CommunityEngine
   module Version
      MAJOR = 3
      MINOR = 0
-     PATCH = 2
-     BUILD = "pre3"
+     PATCH = 0
+     BUILD = nil
 
      STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
    end
