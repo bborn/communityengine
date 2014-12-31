dfdd9167a92488ef32ff5a6490ccae672036d968
Trevor Bullock <trevorjbullock@gmail.com>
2014-12-30 17:59:23 -0800

    Fix autocomplete for messages.

--- config/routes.rb
+++ config/routes.rb
@@ -230,7 +230,7 @@ Rails.application.routes.draw do
 
     resources :favorites
     resources :messages do
-      post :auto_complete_for_username, :on => :collection
+      get :auto_complete_for_username, :on => :collection
       collection do
         post :delete_message_threads
         post :delete_selected
