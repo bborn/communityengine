d9a86cb9e5753010645e4e114afa1da2ffaaa720
Trevor Bullock <trevorjbullock@gmail.com>
2016-03-26 01:55:53 -0400

    Fix config deprecation warnings and test error.

--- test/testapp/config/environments/test.rb
+++ test/testapp/config/environments/test.rb
@@ -31,4 +31,8 @@ Dummy::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.active_support.test_order = :random
+
+  config.active_record.raise_in_transactional_callbacks = true
 end
