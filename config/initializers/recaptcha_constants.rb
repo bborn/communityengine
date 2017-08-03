01a96ab40da93c846d5ffd89b99b27a9dbf702fa
emosei <sei.emoto+github@gmail.com>
2017-08-03 12:06:39 +0900

    recaptcha4.0対応

--- config/initializers/recaptcha_constants.rb
+++ config/initializers/recaptcha_constants.rb
@@ -1,6 +1,6 @@
 if configatron.has_key?(:allow_anonymous_commenting) || configatron.has_key?(:require_captcha_on_signup)
   Recaptcha.configure do |config|
-    config.public_key  = configatron.recaptcha_pub_key
-    config.private_key = configatron.recaptcha_priv_key
+    config.site_key  = configatron.recaptcha_pub_key
+    config.secret_key = configatron.recaptcha_priv_key
   end
 end
